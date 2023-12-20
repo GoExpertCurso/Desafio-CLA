ec55cd3a471e17a43c530d43af0ef42e4a3e78c1
Luciano Santos <luciano_z7@hotmail.com>
2023-12-19 22:29:38 -0300

    Adding generate graph files

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/GoExpertCurso/Desafio-CLA
 
-go 1.21.3
+go 1.19
 
 require (
 	github.com/99designs/gqlgen v0.17.41
---
+++ graph/resolver.go
@@ -0,0 +1,13 @@
+package graph
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+import (
+	"github.com/GoExpertCurso/Desafio-CLA/internal/usecase"
+)
+
+type Resolver struct {
+	CreateOrderUseCase usecase.CreateOrderUseCase
+}
